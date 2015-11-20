3755e983c4b3a810fd565aa6d465ced3f78131b0
Lokesh <lokesh.gahlot@wellsfargo.com>
2015-11-20 06:14:59 -0800

    tiles component added Changes to be committed: new file:   src/tiles/tiles.babelify.js

---
+++ src/tiles/tiles.babelify.js
@@ -0,0 +1,31 @@
+;(function() {
+  'use strict';
+
+  var wrap = function(React, Tile) {
+    return React.createClass({
+      displayName: 'Tiles',
+      render: function() {
+
+        var items = this.props.items.map(function(item, index) {
+          return (
+              <li>
+                <Tile text={item} />
+              </li>
+            );
+        });
+        return (
+          <ul className='tiles' >
+            {items}
+          </ul>
+        );
+      }
+    });
+  };
+
+  if (typeof define === 'function' && define.amd) {
+    define('tiles.babelify', [
+      'react',
+      'tile.babelify'
+      ], wrap);
+  }
+})(this);
