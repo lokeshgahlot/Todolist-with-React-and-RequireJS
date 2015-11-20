086f4b95c0f8a5090006f78b559949947124bca5
Lokesh <lokesh.gahlot@wellsfargo.com>
2015-11-20 06:10:44 -0800

    Firbase logic and other component added. Changes to be committed: modified:   src/main.babelify.js

--- src/main.babelify.js
+++ src/main.babelify.js
@@ -1,11 +1,42 @@
 ;(function() {
   'use strict';
-  var wrap = function(React, ReactDOM, Tile, InputTile) {
+  var wrap = function(React, ReactDOM, Firebase, ReactFire,Tiles, InputTile) {
+    console.log('Firebase = ', Firebase);
+    console.log('ReactFire = ', ReactFire);
+
+
     var Main = React.createClass({
+      mixins: [ReactFire],
+
+      getInitialState: function() {
+        return {
+          items: [],
+          text: ''
+        };
+      },
+      componentWillMount: function() {
+        this.firebaseRef = new Firebase("https://todo-app-react.firebaseio.com/lokesh");
+        this.bindAsArray(firebaseRef.limitToLast(25), 'items');
+      },
+      onChange: function(e) {
+        this.setState({text: e.target.value});
+      },
+      removeItem: function(key) {
+        var firebaseRef = new Firebase('https://ReactFireTodoApp.firebaseio.com/items/');
+        firebaseRef.child(key).remove();
+      },
+      addItem: function() {
+          this.firebaseRefs['items'].push({
+            text: this.state.text
+          });
+          this.setState({
+            text: ''
+          });
+      },
       render: function() {
         return (
           <div className="main">
-            <Tile text='Some Data'/>
+            <Tiles items={[1, 2, 3]} />
             <InputTile />
           </div>
         );
@@ -21,7 +52,9 @@
     define('main.babelify',[
                     'react',
                     'react-dom',
-                    'tile.babelify',
+                    'firebase',
+                    'reactfire',
+                    'tiles.babelify',
                     'input-tile.babelify'
                   ], wrap);
   }
