9c306cad6783906df3e5490f2170ef927e48d262
Lokesh <lokesh.gahlot@wellsfargo.com>
2015-11-20 21:04:26 -0800

    Added logic for adding data in firebase. Changes to be committed: modified:   src/main.babelify.js

--- src/main.babelify.js
+++ src/main.babelify.js
@@ -1,9 +1,6 @@
 ;(function() {
   'use strict';
   var wrap = function(React, ReactDOM, Firebase, ReactFire,Tiles, InputTile) {
-    console.log('Firebase = ', Firebase);
-    console.log('ReactFire = ', ReactFire);
-
 
     var Main = React.createClass({
       mixins: [ReactFire],
@@ -15,50 +12,31 @@
         };
       },
       componentWillMount: function() {
-        // this.firebaseRef = new Firebase("https://todo-app-react.firebaseio.com/lokesh/items");
-        // this.bindAsArray(this.firebaseRef.limitToLast(25), 'items');
+        this.firebaseRef = new Firebase("https://todo-app-react.firebaseio.com/lokesh/items");
+        this.bindAsArray(this.firebaseRef.limitToLast(25), 'items');
       },
       onChange: function(e) {
         this.setState({text: e.target.value});
       },
-      /*removeItem: function(key) {
-        var firebaseRef = new Firebase('https://todo-app-react.firebaseio.com/lokesh/items');
-        firebaseRef.child(key).remove();
-      },
-      addItem: function() {
-          this.firebaseRefs['items'].push({
-            text: this.state.text
+      onSubmit: function(data) {
+        this.firebaseRefs['items'].push({
+            text: data
           });
           this.setState({
             text: ''
           });
-      },*/
-
-      onSubmit: function() {
-        console.log('********>>>>>');
       },
       render: function() {
+        var list = this.state.items.map(function(item ,index) {
+          return {
+            key: item['.key'],
+            text: item['text']
+          };
+        });
 
-        var tempData = [
-          {
-            '.key': '1',
-            '.value': 'todo-1'
-          },
-
-          {
-            '.key': '2',
-            '.value': 'todo-2'
-          },
-
-          {
-            '.key': '3',
-            '.value': 'todo-3'
-          },
-        ];
-        // <Tiles items={ this.state.items } />
         return (
           <div className="main">
-            <Tiles items={ tempData } />
+            <Tiles items={list} />
             <InputTile onSubmit={this.onSubmit}/>
           </div>
         );
