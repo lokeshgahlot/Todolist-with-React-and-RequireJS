da05fe7b6ef32e7aa4ab11c61b020f91aae19b20
Lokesh <lokesh.gahlot@wellsfargo.com>
2015-12-09 22:15:23 -0800

    Key attr added Changes to be committed: modified:   src/tile/tile.babelify.js

--- src/tile/tile.babelify.js
+++ src/tile/tile.babelify.js
@@ -4,10 +4,16 @@
   var wrap = function(React) {
     return React.createClass({
       displayName: 'Tile',
+      deleteMe: function() {
+        // todo: will add remove logic
+        // this.props.removeItem(this.props.key);
+        console.log('click....', this.props);
+      },
       render: function() {
         return (
-          <div className='tile' data-key={this.props.key}>
+          <div className='tile' key={this.props.key} >
             {this.props.text}
+            <i className='icon trash' onClick= {this.deleteMe}/>
           </div>
         );
       }
